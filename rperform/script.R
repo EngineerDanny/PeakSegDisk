e73c0eb1e37b3d23400fdffbefb4e414d5df6de7
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-08-29 22:26:26 -0700

    update script

--- rperform/script.R
+++ rperform/script.R
@@ -6,7 +6,9 @@ Rperform::plot_metrics(
     test_path = "tests/testthat/test-CRAN-wc2int.R",
     metric = "time", num_commits = 50,
     save_data = TRUE,
-    save_plots = TRUE
+    save_plots = TRUE,
+    total_height_in = 30
+
 )
 
 
