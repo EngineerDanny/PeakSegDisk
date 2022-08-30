e301c02b57598ed234cedeee50d670edd5a96282
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-08-29 22:12:01 -0700

    update script

--- rperform/script.R
+++ rperform/script.R
@@ -4,7 +4,7 @@
 ## TEST 1
 Rperform::plot_metrics(
     test_path = "tests/testthat/test-CRAN-cpp-errors.R",
-    metric = "time", num_commits = 5,
+    metric = "time", num_commits = 50,
     save_data = TRUE,
     save_plots = TRUE
 )
